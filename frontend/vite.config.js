5c91eeb59082d9b7c7d954c106e8c09fb4c4d6ed
Mirel <gugila.mirelandrei@gmail.com>
2023-07-03 16:10:15 +0300

    fix react

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    // DOCKER HOST PORT
-    host: '0.0.0.0',
-  },
-})
+	plugins: [react()],
+	server: {
+		port: 3000,
+		// DOCKER HOST PORT
+		host: '0.0.0.0',
+	},
+});
