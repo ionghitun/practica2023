dfb299b7c789fc8cca4ace831cf541c5395ed116
Mirel <gugila.mirelandrei@gmail.com>
2023-07-05 09:49:23 +0300

    add error handle

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { Suspense } from 'react';
 import { Provider } from 'react-redux';
+import { MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
 import Router from './router';
 import store from './state/store';
-import { MantineProvider } from '@mantine/core';
 
 function App() {
 	return (
 		<Provider store={store}>
 			<MantineProvider withGlobalStyles withNormalizeCSS>
+				<Notifications />
 				<Suspense fallback={<div>loading...</div>}>
 					<Router />
 				</Suspense>
--- frontend/src/state/errorHandler.jsx
+++ frontend/src/state/errorHandler.jsx
@@ -1,17 +1,16 @@
 import { isRejectedWithValue } from '@reduxjs/toolkit';
+import { notifications } from '@mantine/notifications';
+import { IconExclamationMark } from '@tabler/icons-react';
+import { Text } from '@mantine/core';
 
-function renderErrorMessage(errorMessage) {
-	if (typeof errorMessage === 'string') {
-		// Render a new line for a string
-		return errorMessage;
-	} else if (typeof errorMessage === 'object' && errorMessage !== null) {
-		// Render a new line for each value in the object
-		return Object.values(errorMessage).map((value) => {
-			return <div>{value}</div>;
-		});
-	} else {
-		console.log('Invalid error message format');
-	}
+function ErrorMessage({ messages = [] }) {
+	return (
+		<div>
+			{messages?.map((message) => (
+				<Text key={message}>{message}</Text>
+			))}
+		</div>
+	);
 }
 
 /**
@@ -20,25 +19,30 @@ function renderErrorMessage(errorMessage) {
 const rtkQueryErrorLogger = () => (next) => (action) => {
 	// RTK Query uses `createAsyncThunk` from redux-toolkit under the hood, so we're able to utilize these matchers!
 	if (isRejectedWithValue(action)) {
-		if (action.payload.status === 401 && action.payload.data.errorMessage === 'unauthenticated') {
-			// store.dispatch(api.util.resetApiState());
-			// if (window.location.pathname !== '/login') {
-			// 	window.location.href = '/login';
-			// }
+		if (action.payload.status === 401 && action.payload.data.message === 'Unauthenticated.') {
 			return next(action);
 		}
 
-		if (action.payload.status === 401 && action.payload.data.errorMessage === 'unauthorized') {
+		if (action.payload.status === 401 && action.payload.data.message === 'unauthorized') {
 			window.location.href = '/unauthorized';
 		}
 
-		// return false;
-		// notifications.show({
-		// 	title: 'Eroare',
-		// 	message: renderErrorMessage(action.payload.data.errorMessage),
-		// 	color: 'red',
-		// 	icon: <IconExclamationMark />,
-		// });
+		let errorMessages = [];
+
+		if (action.payload?.data?.message) {
+			errorMessages = [action.payload.data.message];
+		}
+
+		if (action?.payload?.data?.errors && Object.values(action.payload.data.errors)?.flat()) {
+			errorMessages = Object.values(action.payload.data.errors).flat();
+		}
+
+		notifications.show({
+			title: 'Error',
+			message: <ErrorMessage messages={errorMessages} />,
+			color: 'red',
+			icon: <IconExclamationMark />,
+		});
 
 		console.warn('We got a rejected action!', action);
 	}
